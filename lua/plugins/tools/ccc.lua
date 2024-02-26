f66bd60c1ed4161ac48ba6f44e93eff9c0f31293
memmoxt <willianfujii@gmail.com>
2024-02-26 16:55:21 -0300

    new ccc config

--- lua/plugins/tools/ccc.lua
+++ lua/plugins/tools/ccc.lua
@@ -1,27 +1,21 @@
 return {
     "uga-rosa/ccc.nvim",
-    opts = function()
+    config = function()
         local ccc = require("ccc")
         local mapping = ccc.mapping
-
+        local utils = require("ccc.utils")
         vim.opt.termguicolors = true
+
         ccc.setup({
             mappings = {
-                ["q"] = mapping.quit,
                 ["<CR>"] = mapping.complete,
-                ["i"] = mapping.toggle_input_mode,
-                ["o"] = mapping.toggle_output_mode,
-                ["r"] = mapping.reset_mode,
-                ["a"] = mapping.toggle_alpha,
-                ["g"] = mapping.toggle_prev_colors,
-                ["w"] = mapping.goto_next,
-                ["b"] = mapping.goto_prev,
-                ["W"] = mapping.goto_tail,
-                ["B"] = mapping.goto_head,
+                ["q"] = mapping.quit,
+                ["l"] = mapping.increase1,
                 ["<Right>"] = mapping.increase1,
                 ["<S-Right>"] = mapping.increase10,
                 ["d"] = mapping.increase5,
                 [","] = mapping.increase10,
+                ["h"] = mapping.decrease1,
                 ["<Left>"] = mapping.decrease1,
                 ["<S-Left>"] = mapping.decrease10,
                 ["s"] = mapping.decrease5,
@@ -30,31 +24,27 @@ return {
                 ["M"] = mapping.set50,
                 ["L"] = mapping.set100,
                 ["0"] = mapping.set0,
-                ["1"] = function()
-                    ccc.set_percent(10)
-                end,
-                ["2"] = function()
-                    ccc.set_percent(20)
-                end,
-                ["3"] = function()
-                    ccc.set_percent(30)
-                end,
-                ["4"] = function()
-                    ccc.set_percent(40)
-                end,
+                ["1"] = utils.bind(mapping._set_percent, 10),
+                ["2"] = utils.bind(mapping._set_percent, 20),
+                ["3"] = utils.bind(mapping._set_percent, 30),
+                ["4"] = utils.bind(mapping._set_percent, 40),
                 ["5"] = mapping.set50,
-                ["6"] = function()
-                    ccc.set_percent(60)
-                end,
-                ["7"] = function()
-                    ccc.set_percent(70)
-                end,
-                ["8"] = function()
-                    ccc.set_percent(80)
-                end,
-                ["9"] = function()
-                    ccc.set_percent(90)
-                end,
+                ["6"] = utils.bind(mapping._set_percent, 60),
+                ["7"] = utils.bind(mapping._set_percent, 70),
+                ["8"] = utils.bind(mapping._set_percent, 80),
+                ["9"] = utils.bind(mapping._set_percent, 90),
+                ["r"] = mapping.reset_mode,
+                ["a"] = mapping.toggle_alpha,
+                ["g"] = mapping.toggle_prev_colors,
+                ["b"] = mapping.goto_prev,
+                ["w"] = mapping.goto_next,
+                ["B"] = mapping.goto_head,
+                ["W"] = mapping.goto_tail,
+                ["i"] = mapping.cycle_input_mode,
+                ["o"] = mapping.cycle_output_mode,
+                ["<LeftMouse>"] = mapping.click,
+                ["<ScrollWheelDown>"] = mapping.decrease1,
+                ["<ScrollWheelUp>"] = mapping.increase1,
             },
         })
     end,

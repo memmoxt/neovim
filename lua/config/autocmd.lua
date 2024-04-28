849f6bee93622c33fb759c6f3df53a5aad3a6f3e
memmoxt <willianfujii@gmail.com>
2024-04-28 11:15:05 -0300

    yank hightlight

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -5,3 +5,10 @@ vim.cmd [[
   autocmd FileType html,javascript setlocal tabstop=2 shiftwidth=2 expandtab
 ]]
 
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd [[
+augroup highlight_yank
+autocmd!
+au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+augroup END
+]]

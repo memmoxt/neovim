3f6ea911b061ada7cf9d79e9b15bae2322468499
memmoxt <willianfujii@gmail.com>
2024-06-27 21:28:11 -0300

    trouble update

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -38,8 +38,5 @@ return {
             row = 0,
             col = 1
         },
-        yadm                         = {
-            enable = false
-        },
     },
 }
--- lua/plugins/tools/trouble.lua
+++ lua/plugins/tools/trouble.lua
@@ -1,86 +1,40 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{ "<leader>x", desc = "+Trouble" },
-		{
-			"<leader>xx",
-			function()
-				require("trouble").open()
-			end,
-			mode = "n",
-			desc = "Trouble Open",
-		},
-
-		{
-			"<leader>xw",
-			function()
-				require("trouble").open("workspace_diagnostics")
-			end,
-			mode = "n",
-			desc = "TroubleToggle Workspace Diagnostics",
-		},
-
-		{
-			"<leader>xd",
-			function()
-				require("trouble").open("document_diagnostics")
-			end,
-			mode = "n",
-			desc = "TroubleToggle Documents Diagnostics",
-		},
-
-		{
-			"<leader>xq",
-			function()
-				require("trouble").open("quickfix")
-			end,
-			mode = "n",
-			desc = "TroubleToggle Quickfix",
-		},
-
-		{
-			"<leader>xl",
-			function()
-				require("trouble").open("loclist")
-			end,
-			mode = "n",
-			desc = "TroubleToggle Loclist",
-		},
-
-		-- {
-		-- 	"gR",
-		-- 	function()
-		-- 		require("trouble").open("lsp_references")
-		-- 	end,
-		-- 	mode = "n",
-		-- 	desc = "Trouble LSP references",
-		-- },
-	},
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	config = function()
-		local trouble = require("trouble.providers.telescope")
-
-		local telescope = require("telescope")
-
-		telescope.setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<c-t>"] = function(...)
-							return require("trouble.providers.telescope").open_with_trouble(...)
-						end,
-						["<a-t>"] = function(...)
-							return require("trouble.providers.telescope").open_selected_with_trouble(...)
-						end,
-					},
-					n = { ["<c-t>"] = trouble.open_with_trouble },
-				},
-			},
-		})
-	end,
+    "folke/trouble.nvim",
+    lazy = false,
+    cmd = "Trouble",
+    config = function()
+        require("trouble").setup()
+    end,
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
 }
